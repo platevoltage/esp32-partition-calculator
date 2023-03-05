6fa24bf6a1de5f71ef2e59e1320799252e9e384d
antieatingactivist <antieatingactivist@gmail.com>
2023-03-04 22:21:32 -0800

    set up state

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -10,11 +10,13 @@ export interface Partition {
 }
 
 interface Props {
-    data: Partition;
+    table: Partition[];
+    setTable: (data: Partition[]) => void;
+    i: number;
 }
 
 
-export default function Row({data}: Props) {
+export default function Row({table, setTable, i}: Props) {
   return (
     <div className="row">
 
@@ -25,38 +27,44 @@ export default function Row({data}: Props) {
         </div>
 
         <div className="column">
-            <select name="data">
-                <option value="data" selected={data.type === "data"}>data</option>
-                <option value="app" selected={data.type === "app"}>app</option>
+            <select name="type" value={table[i].type} onChange={(e) => {
+                table[i].type = e.target.value;
+                setTable([...table]);
+            }}>
+                <option value="data">data</option>
+                <option value="app">app</option>
             </select>
         </div>
 
         <div className="column">
-            <select name="sub-type">
-                {data.type === "app" ? <>
-                    <option value="factory" selected={data.subType === "factory"}>factory</option>
-                    <option value="test" selected={data.subType === "test"}>test</option>
-                    <option value="ota_0" selected={data.subType === "ota_0"}>ota_0</option>
-                    <option value="ota_1" selected={data.subType === "ota_1"}>ota_1</option>
-                    <option value="ota_2" selected={data.subType === "ota_2"}>ota_2</option>
-                    <option value="ota_3" selected={data.subType === "ota_3"}>ota_3</option>
-                    <option value="ota_4" selected={data.subType === "ota_4"}>ota_4</option>
-                    <option value="ota_5" selected={data.subType === "ota_5"}>ota_5</option>
-                    <option value="ota_6" selected={data.subType === "ota_6"}>ota_6</option>
-                    <option value="ota_7" selected={data.subType === "ota_7"}>ota_7</option>
-                    <option value="ota_8" selected={data.subType === "ota_8"}>ota_8</option>
-                    <option value="ota_9" selected={data.subType === "ota_9"}>ota_9</option>
-                    <option value="ota_10" selected={data.subType === "ota_10"}>ota_10</option>
-                    <option value="ota_11" selected={data.subType === "ota_11"}>ota_11</option>
-                    <option value="ota_12" selected={data.subType === "ota_12"}>ota_12</option>
-                    <option value="ota_13" selected={data.subType === "ota_13"}>ota_13</option>
-                    <option value="ota_14" selected={data.subType === "ota_14"}>ota_14</option>
-                    <option value="ota_15" selected={data.subType === "ota_15"}>ota_15</option>
+            <select name="sub-type" value={table[i].subType} onChange={(e) => {
+                table[i].subType = e.target.value;
+                setTable([...table]);
+            }}>
+                {table[i].type === "app" ? <>
+                    <option value="factory">factory</option>
+                    <option value="test">test</option>
+                    <option value="ota_0">ota_0</option>
+                    <option value="ota_1">ota_1</option>
+                    <option value="ota_2">ota_2</option>
+                    <option value="ota_3">ota_3</option>
+                    <option value="ota_4">ota_4</option>
+                    <option value="ota_5">ota_5</option>
+                    <option value="ota_6">ota_6</option>
+                    <option value="ota_7">ota_7</option>
+                    <option value="ota_8">ota_8</option>
+                    <option value="ota_9">ota_9</option>
+                    <option value="ota_10">ota_10</option>
+                    <option value="ota_11">ota_11</option>
+                    <option value="ota_12">ota_12</option>
+                    <option value="ota_13">ota_13</option>
+                    <option value="ota_14">ota_14</option>
+                    <option value="ota_15">ota_15</option>
                 </> :
                 <>
-                    <option value="ota" selected={data.subType === "ota"}>ota</option>
-                    <option value="phy" selected={data.subType === "phy"}>phy</option>
-                    <option value="nvs" selected={data.subType === "nvs"}>nvs</option>
+                    <option value="ota">ota</option>
+                    <option value="phy">phy</option>
+                    <option value="nvs">nvs</option>
                 </>
                 }
 
@@ -68,21 +76,21 @@ export default function Row({data}: Props) {
             <input type="text" name="offset">
 
             </input>
-            0x{data.offset.toString(16)}
+            0x{table[i].offset.toString(16)}
         </div>
 
         <div className="column">
             <input type="text" name="size">
 
             </input>
-            0x{data.size.toString(16)}
+            0x{table[i].size.toString(16)}
         </div>
 
         <div className="column">
             <input type="text" name="flags">
 
             </input>
-            {data.flags}
+            {table[i].flags}
         </div>
 
     </div>
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import './Table.css';
 import Row, {Partition} from './Row';
 
 
 export default function Table() {
+    const [table, setTable] = useState<Partition[]>([
+        {
+            name: "nvs", 
+            type: "data", 
+            subType: "nvs", 
+            offset: 0x9000, 
+            size: 0x5000,
+        },
+        {
+            name: "nvs", 
+            type: "app", 
+            subType: "nvs", 
+            offset: 0x9000, 
+            size: 0x5000,
+        },
 
-    const testData: Partition = {
-        name: "nvs", 
-        type: "data", 
-        subType: "nvs", 
-        offset: 0x9000, 
-        size: 0x5000,
-    }
+    ]);
+
     return (
         <main>
             <header className="row">
@@ -21,8 +32,11 @@ export default function Table() {
                 <div className="column">Size</div>
                 <div className="column">Flags</div>
             </header>
-
-            <Row data={testData}/>
+            {
+                table.map((partition, i) => {
+                    return <Row table={table} setTable={setTable} i={i} key={i}/>
+                })
+            }
 
         </main>
     )
