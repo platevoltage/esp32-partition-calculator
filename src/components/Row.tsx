daf92a7d7cb5eb8f91af000a2f1c595dbd32ca20
antieatingactivist <antieatingactivist@gmail.com>
2023-03-04 19:29:46 -0800

    props now passed to row

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 
-export default function Row() {
+export interface Partition {
+    name: string;
+    type: string;
+    subType: string;
+    offset: number;
+    size: number;
+    flags?: string;
+}
+
+interface Props {
+    data: Partition;
+}
+
+
+export default function Row({data}: Props) {
   return (
     <div className="row">
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
+        <div className="column">{data.name}</div>
+        <div className="column">{data.type}</div>
+        <div className="column">{data.subType}</div>
+        <div className="column">0x{data.offset.toString(16)}</div>
+        <div className="column">0x{data.size.toString(16)}</div>
+        <div className="column">{data.flags}</div>
     </div>
   )
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,29 @@
-import React from 'react'
 import './Table.css';
-import Row from './Row';
+import Row, {Partition} from './Row';
+
 
 export default function Table() {
-  return (
-    <main>
-      <header className="row">
-        <div className="column">Name</div>
-        <div className="column">Type</div>
-        <div className="column">SubType</div>
-        <div className="column">Offset</div>
-        <div className="column">Size</div>
-        <div className="column">Flags</div>
-      </header>
-      <Row />
-    </main>
-  )
+
+    const testData: Partition = {
+        name: "nvs", 
+        type: "data", 
+        subType: "nvs", 
+        offset: 0x9000, 
+        size: 0x5000,
+    }
+    return (
+        <main>
+            <header className="row">
+                <div className="column">Name</div>
+                <div className="column">Type</div>
+                <div className="column">SubType</div>
+                <div className="column">Offset</div>
+                <div className="column">Size</div>
+                <div className="column">Flags</div>
+            </header>
+
+            <Row data={testData}/>
+
+        </main>
+    )
 }
