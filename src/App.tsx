a45d062c01dfc7d971bc4c30b00e86cf2149235e
antieatingactivist <antieatingactivist@gmail.com>
2023-03-05 01:16:52 -0800

    added flash size box

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Table from './components/Table';
 
 function App() {
+  const [flashSize, setFlashSize] = useState<number>(4096);
   return (
     <div className="App">
-      <Table />
+      <div style={{ display: 'flex', flexDirection: 'column'}}>
+        <input type="text" style={{width: "10em"}} onChange={(e) =>  setFlashSize(+e.target.value) }></input><br></br>
+        <Table flashSize={flashSize} />
+      </div>
     </div>
   );
 }
