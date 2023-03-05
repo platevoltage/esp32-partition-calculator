3409cef9066c3108e73bd5a1895422d6389ef9d5
antieatingactivist <antieatingactivist@gmail.com>
2023-03-04 19:17:02 -0800

    created basic table components

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Table.css';
+import Row from './Row';
 
 export default function Table() {
   return (
-    <div>
-      bcvbcv
-    </div>
+    <main>
+      <header className="row">
+        <div className="column">Name</div>
+        <div className="column">Type</div>
+        <div className="column">SubType</div>
+        <div className="column">Offset</div>
+        <div className="column">Size</div>
+        <div className="column">Flags</div>
+      </header>
+      <Row />
+    </main>
   )
 }
